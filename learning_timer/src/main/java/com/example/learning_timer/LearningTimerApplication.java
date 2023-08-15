51bd2ce243590b9aca14fe8b2b7a83d95bc25508
Takthustra <aiosu.sp@gmail.com>
2023-08-16 08:05:20 +0900

    learning_timerリポジトリの作成

---
+++ learning_timer/src/main/java/com/example/learning_timer/LearningTimerApplication.java
@@ -0,0 +1,13 @@
+package com.example.learning_timer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningTimerApplication.class, args);
+	}
+
+}
---
+++ learning_timer/src/test/java/com/example/learning_timer/LearningTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learning_timer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningTimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
