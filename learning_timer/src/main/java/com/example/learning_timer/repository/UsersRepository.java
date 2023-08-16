11dc20a1a255cf0f0e97bcf96c534f56d2422cd7
Takthustra <aiosu.sp@gmail.com>
2023-08-16 22:43:37 +0900

    UsersRepositoryにユーザ登録、ユーザ重複確認、ログインメソッドを追加

--- learning_timer/src/main/java/com/example/learning_timer/repository/UsersRepository.java
+++ learning_timer/src/main/java/com/example/learning_timer/repository/UsersRepository.java
@@ -8,8 +8,16 @@ import com.example.learning_timer.entity.Users;
 
 /** Usersテーブル:RepositoryImpl */
 public interface UsersRepository extends CrudRepository<Users,Integer>{
-	//user登録
+	//ユーザ登録
 	@Modifying
 	@Query("insert into users (name,password) values (:name,:password)")
-	void saveUser(String name,String password);
+	void createUser(String name,String password);
+	
+	//ユーザ重複確認
+	@Query("SELECT NAME FROM USERS WHERE NAME = :name")
+	String uniqueUser(String name);
+	
+	//ログイン
+	@Query("SELECT * FROM USERS WHERE NAME = :name AND PASSWORD = :password")
+	Users login(String name,String password);
 }
