3ee5f48bfea7fe3b9f00c39a6f1023329fc85d38
Takthustra <aiosu.sp@gmail.com>
2023-08-16 17:41:58 +0900

    ユーザ登録をQueryアノテーションで記載

--- learning_timer/src/main/java/com/example/learning_timer/repository/UsersRepository.java
+++ learning_timer/src/main/java/com/example/learning_timer/repository/UsersRepository.java
@@ -1,10 +1,15 @@
 package com.example.learning_timer.repository;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.learning_timer.entity.Users;
 
 /** Usersテーブル:RepositoryImpl */
 public interface UsersRepository extends CrudRepository<Users,Integer>{
-	
+	//user登録
+	@Modifying
+	@Query("insert into users (name,password) values (:name,:password)")
+	void saveUser(String name,String password);
 }
