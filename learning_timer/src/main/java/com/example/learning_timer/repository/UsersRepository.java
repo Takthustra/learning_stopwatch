1721b2de47b4fbe2fc19a54eaed47019bc5772e8
Takthustra <aiosu.sp@gmail.com>
2023-08-17 09:30:58 +0900

    @Paramでの指定に変更

--- learning_timer/src/main/java/com/example/learning_timer/repository/UsersRepository.java
+++ learning_timer/src/main/java/com/example/learning_timer/repository/UsersRepository.java
@@ -3,6 +3,7 @@ package com.example.learning_timer.repository;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.learning_timer.entity.Users;
 
@@ -11,13 +12,13 @@ public interface UsersRepository extends CrudRepository<Users,Integer>{
 	//ユーザ登録
 	@Modifying
 	@Query("insert into users (name,password) values (:name,:password)")
-	void createUser(String name,String password);
+	void createUser(@Param("name") String name,@Param("password") String password);
 	
 	//ユーザ重複確認
 	@Query("SELECT NAME FROM USERS WHERE NAME = :name")
-	String uniqueUser(String name);
+	String uniqueUser(@Param("name") String name);
 	
 	//ログイン
 	@Query("SELECT * FROM USERS WHERE NAME = :name AND PASSWORD = :password")
-	Users login(String name,String password);
+	Users login(@Param("name") String name,@Param("password") String password);
 }
