46640822e99a48843741d0ad24637abffee6f8fb
Takthustra <aiosu.sp@gmail.com>
2023-08-18 10:18:43 +0900

    bin\main\templatesの生成テスト

---
+++ learning_timer/src/main/java/com/example/learning_timer/controller/TestController.java
@@ -0,0 +1,44 @@
+package com.example.learning_timer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+/** Userコントローラ */
+
+import com.example.learning_timer.form.CreateUserForm;
+import com.example.learning_timer.service.UsersService;
+@Controller
+@RequestMapping("tst")
+public class TestController{
+    /** DI対象 */
+    @Autowired
+    UsersService service;
+    @ModelAttribute
+    public CreateUserForm setUpForm(){
+        CreateUserForm form = new CreateUserForm();
+        return form;
+    }
+    @GetMapping("create")
+    public String showCreateUser(CreateUserForm form,Model model){
+
+        return "create";
+    }
+    @GetMapping("hello1")
+    public String showHello(CreateUserForm form,Model model){
+
+        return "hello1";
+    }
+    @GetMapping("apple")
+    public String showApple(CreateUserForm form,Model model){
+
+        return "apple";
+    }
+    @GetMapping("orange")
+    public String showOrange(CreateUserForm form,Model model){
+
+        return "orange";
+    }
+}
