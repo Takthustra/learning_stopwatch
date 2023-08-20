791dc76643359eca3799d40801c6d0672d65f111
Takthustra <aiosu.sp@gmail.com>
2023-08-21 07:45:02 +0900

    フォルダ階層、コメントの記載

--- learning_timer/src/main/java/com/example/learning_timer/controller/MainController.java
+++ learning_timer/src/main/java/com/example/learning_timer/controller/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.learning_timer.form.UserForm;
@@ -26,10 +27,26 @@ public class MainController {
 
 		//ログイン時のsessionが生成されているか確認
 		if (name != null) {
+			return "main/top";
+		} else {
+			return "redirect:/user/login";
+		}
+		
+	}
+	
+	@PostMapping
+	public String postMain(UserForm form, Model model) {
+		String name = (String) session.getAttribute("name");
+		
+		System.out.println("ポストされました");
+		
+		//ログイン時のsessionが生成されているか確認
+		if (name != null) {
 			return "main";
 		} else {
 			return "redirect:/user/login";
 		}
-
+		
 	}
+	
 }
