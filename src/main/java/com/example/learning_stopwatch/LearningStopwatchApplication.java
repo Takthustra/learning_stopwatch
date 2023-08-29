2d20a1fdc8328d8d55c86094775640dffc623eb9
Takthustra <aiosu.sp@gmail.com>
2023-08-29 22:53:40 +0900

    build用に変更

--- src/main/java/com/example/learning_stopwatch/LearningStopwatchApplication.java
+++ src/main/java/com/example/learning_stopwatch/LearningStopwatchApplication.java
@@ -2,13 +2,21 @@ package com.example.learning_stopwatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LearningStopwatchApplication {
+public class LearningStopwatchApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningStopwatchApplication.class, args);
 	}
 	
+	// ここから追加
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	return application.sources(LearningStopwatchApplication.class);
+	}
+	// ここまで追加
 
 }
