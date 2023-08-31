91da8e892a2cef8ebd7a6211545c75a986ef3713
Takthustra <aiosu.sp@gmail.com>
2023-08-31 16:55:14 +0900

    index.htmlにログインの有無によってナビゲーションバーの表示の仕方が変わる機能を追加

--- src/main/java/com/example/learning_stopwatch/controller/IndexController.java
+++ src/main/java/com/example/learning_stopwatch/controller/IndexController.java
@@ -1,18 +1,29 @@
 package com.example.learning_stopwatch.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.learning_stopwatch.entity.User;
+
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping()
 public class IndexController {
+	@Autowired
+	HttpSession session;
 
 	@GetMapping()
 	public String getIndex(Model model) {
-		model.addAttribute("catchphrase1","君の学びの軌跡を刻む。");
-		model.addAttribute("catchphrase2","さぁ、今から始めよう。");
+		//ログイン時のsessionが生成されているか確認
+		User user = (User) session.getAttribute("user");
+		
+		if(user != null) {
+			model.addAttribute("boolean",true);
+		}
 		
 		return "index/index";
 
